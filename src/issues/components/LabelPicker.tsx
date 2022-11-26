35b88bbece6e2877e2d8571c1b1e71ee021fa66d
Kelvia Santos <kelviaks@gmail.com>
2022-11-26 11:17:12 -0300

    initialData x placeholderData and add function to select tags

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,7 +1,13 @@
+import { FC } from "react";
 import { LoadingIcons } from "../../shared/components/LoadingIcons";
 import { useLabels } from "../hooks/useLabels";
 
-export const LabelPicker = () => {
+interface Props {
+  selectLabels: string[];
+  onChange: (labelName: string) => void;
+}
+
+export const LabelPicker: FC<Props> = ({ selectLabels, onChange }) => {
   const labelsQuery = useLabels();
 
   if (labelsQuery.isLoading) return <LoadingIcons />;
@@ -11,11 +17,14 @@ export const LabelPicker = () => {
       {labelsQuery.data?.map((label) => (
         <span
           key={label.id}
-          className="badge rounded-pill m-1 label-picker"
+          className={`badge rounded-pill m-1 label-picker ${
+            selectLabels.includes(label.name) ? "label-active" : ""
+          }`}
           style={{
             border: `1px solid #${label.color}`,
             color: `#${label.color} `,
           }}
+          onClick={() => onChange(label.name)}
         >
           {label.name}
         </span>
--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -13,6 +13,25 @@ const getLabels = async (): Promise<Label[]> => {
 export const useLabels = () => {
   const LabelsQuery = useQuery(["labels"], getLabels, {
     staleTime: 1000 * 60 * 60, // 1 hour - stale(0)
+    // initialData: [],
+    placeholderData: [
+      {
+        id: 725156255,
+        node_id: "MDU6TGFiZWw3MjUxNTYyNTU=",
+        url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue%20(taken)",
+        name: "good first issue (taken)",
+        color: "b60205",
+        default: false,
+      },
+      {
+        id: 717031390,
+        node_id: "MDU6TGFiZWw3MTcwMzEzOTA=",
+        url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue",
+        name: "good first issue",
+        color: "6ce26a",
+        default: true,
+      },
+    ],
   });
 
   return LabelsQuery;
