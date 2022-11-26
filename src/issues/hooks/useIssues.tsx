26555380575f5ff7a23f59940617fdbe45868fd4
Kelvia Santos <kelviaks@gmail.com>
2022-11-26 13:13:25 -0300

    Upload Issues from GitHub

---
+++ src/issues/hooks/useIssues.tsx
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import { githubApi } from "../../api/githuApi";
+import { Issue } from "../interfaces";
+
+const getIssues = async (): Promise<Issue[]> => {
+  const { data } = await githubApi.get<Issue[]>("/issues");
+  console.log(data);
+
+  return data;
+};
+
+export const useIssues = () => {
+  const issuesQuery = useQuery(["issues"], getIssues);
+
+  return { issuesQuery };
+};
--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { IssueList } from "../components/IssueList";
 import { LabelPicker } from "../components/LabelPicker";
+import { useIssues } from "../hooks";
 
 export const ListView = () => {
   const [selectLabels, setSelectLabels] = useState<string[]>([]);
+  const { issuesQuery } = useIssues();
 
   const onchangeLabel = (labelName: string) => {
     selectLabels.includes(labelName)
