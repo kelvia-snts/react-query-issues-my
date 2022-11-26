79fc8a1bfe714da713bf260e25cde17f1abaf28d
Kelvia Santos <kelviaks@gmail.com>
2022-11-26 09:28:30 -0300

    call project API

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,14 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+
+const getLabels = async () => {
+  const res = await fetch("https://api.github.com/repos/facebook/react/labels");
+  const data = await res.json();
+  console.log(data);
+  return data;
+};
 
 export const LabelPicker = () => {
+  const LabelsQuery = useQuery(["labels"], getLabels);
+
   return (
     <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-        >
-            Primary
-        </span>
-        
+      <span
+        className="badge rounded-pill m-1 label-picker"
+        style={{ border: `1px solid #ffccd3`, color: "#ffccd3" }}
+      >
+        Primary
+      </span>
     </div>
-  )
-}
+  );
+};
